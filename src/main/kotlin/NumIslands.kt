1a4abd4b8a6000eb16850986075f6ca8dd0172d1
Murillo Peixoto <>
2025-11-09 22:43:36 +0100

    Resolve Number of Islands

--- src/main/kotlin/NumIslands.kt
+++ src/main/kotlin/NumIslands.kt
@@ -1,7 +1,5 @@
 package org.example
 
-import java.util.stream.IntStream.range
-
 /**
  * Solves problem: Number of Islands
  * https://leetcode.com/problems/number-of-islands/description/
@@ -11,15 +9,54 @@ import java.util.stream.IntStream.range
 fun numIslands(grid: Array<CharArray>): Int {
     var numIslands = 0
     if (grid.isEmpty()) return numIslands
-    val rows = grid.size
-    val cols = grid[0].size
-    val visited: MutableSet<Pair<Int, Int>> = mutableSetOf<Pair<Int, Int>>()
-
-    for (row in range(0, rows)) {
-        for (col in range(0, cols)) {
-            if ((grid[row][col] == '1') && !visited.contains(row to col)) {
-                numIslands++
+    val lastRow = grid.lastIndex
+    val lastColumn = grid[0].lastIndex
+    val visited: MutableSet<Pair<Int, Int>> = mutableSetOf()
+    val queue = ArrayDeque<Pair<Int, Int>>()
+    queue.addLast(0 to 0)
+    while (queue.isNotEmpty()) {
+        val node = queue.removeFirst()
+        if (!visited.contains(node) && grid[node.first][node.second] == '1') {
+            numIslands += 1
+        }
+        visited.add(node)
+        if (grid[node.first][node.second] == '1') {
+            val subQueue = ArrayDeque<Pair<Int, Int>>()
+            if (node.first + 1 <= lastRow) {
+                subQueue.addLast(node.first + 1 to node.second)
             }
+            if (node.second + 1 <= lastColumn) {
+                subQueue.addLast(node.first to node.second + 1)
+            }
+            while (subQueue.isNotEmpty()) {
+                val sameIslandNode = subQueue.removeFirst()
+                if (visited.contains(sameIslandNode)) {
+                    continue
+                }
+                visited.add(sameIslandNode)
+                if (grid[sameIslandNode.first][sameIslandNode.second] == '0') {
+                    continue
+                }
+                if (sameIslandNode.first - 1 >= 0) {
+                    subQueue.addLast(sameIslandNode.first - 1 to sameIslandNode.second)
+                }
+                if (sameIslandNode.second - 1 >= 0) {
+                    subQueue.addLast(sameIslandNode.first to sameIslandNode.second - 1)
+                }
+                if (sameIslandNode.first + 1 <= lastRow) {
+                    subQueue.addLast(sameIslandNode.first + 1 to sameIslandNode.second)
+                }
+                if (sameIslandNode.second + 1 <= lastColumn) {
+                    subQueue.addLast(sameIslandNode.first to sameIslandNode.second + 1)
+                }
+            }
+        }
+
+        if (node.first + 1 <= lastRow && !queue.contains(node.first + 1 to node.second)) {
+            queue.addLast(node.first + 1 to node.second)
+        }
+        if (node.second + 1 <= lastColumn && !queue.contains(node.first to node.second + 1)) {
+            queue.addLast(node.first to node.second + 1)
         }
     }
 
